48e4840dfc246378a6bfdf555d7c3143929c8674
es765131 <es765@kent.ac.uk>
2024-11-10 12:42:35 +0000

    commiting!!

--- assign1.c
+++ assign1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-void program1();
+void program1(); //Define our functions early, so that the compiler knows the other two functions exist when they are called in main().
 void program2();
 void main();
 
@@ -8,21 +8,21 @@ void main()
 {
     char select[50];
 
-    printf("\nChoose a program (Type 1-2)...\n");
+    printf("\nChoose a program (Type 1-2)...\n"); // Print information to the console, \n adds a line break.
     scanf("%s",&select); // Wait for user input, then set it aside in the select char array.
-    switch (select[0])
+    switch (select[0]) //Switch statement is best for this scenario, especially if we plan to add more programs later on.
     {
     case '1': // If the first char is 1, then true.
         program1(); // Execute program 1.
-        break;
+        break; // Break out of the switch case.
 
     case '2':
         program2();
         break;
 
-    default:
+    default: // Acts as input validation. If anything other than 1 or 2 is entered as the first character, ask the user again.
         printf("\nUnknown command.\n");
-        main();
+        main(); // This sends us back to the start of main() function.
         break;
     }
     return;
@@ -37,28 +37,30 @@ void program1()
 
     scanf("%d", &limit); // Recieve number from user, store in limit variable
 
-    // Define array (two dimensional)
+    // Define int array (two dimensional) and our two iterators.
     int table[limit][limit];
     int i;
     int j;
-    for (i = 1; i < (limit + 1); i++)
-    {
-        for (j = 1; j < (limit + 1); j++)
+    for (i = 1; i < (limit + 1); i++)   // For loop, iterates through the rows of our array. 
+    {                                   // We add one to the limit so that i is equal to the limit before the loop ends.
+                                        // Otherwise, the loop will stop one literation earlier than intended.
+        for (j = 1; j < (limit + 1); j++) // Another for loop, nested in the first. This iterates through the columns of our array.
         {
             table[i][j] = (j * i); // Fill our array with values using for loops
-            // attempting to build an array larger than 17 causes a 'segmentation fault', i'm not sure why it is happening.
+            // attempting to build an array larger than 17 causes a 'segmentation fault', i'm not sure why it is happening. A 16x16 array is good enough.
         }
         
     }
     
     i = 1;
     j = 1;
-    while (i < (limit + 1))
-    {
+    while (i < (limit + 1))     // Similar to the for loops above, just using a while loop this time.
+    {                           // The only real difference is that we must give our iterators a value before the while loop, and must iterate the iterator ourselves.
         while (j < (limit + 1))
         {
             printf("%d ", table[i][j]); //printing the array to the screen, using while loops this time
-            j++;
+
+            j++; // Iterate our iterator.
         }
         printf("\n");
         i++;
@@ -71,26 +73,26 @@ void program1()
 
 void program2()
 {
-    float num1;
+    float num1; // Define our floats
     float num2;
 
     printf("\nDivision program.\nEnter a number... (For example, type 20.055)\n");
     scanf("%f", &num1);
     printf("\nEnter another number... (For example, type 1.5)\n");
-    scanf("%f", &num2);
+    scanf("%f", &num2); // Gather user input. %f is used for float variables.
 
-    printf("%f divided by %f is:\n", num1,num2);
-    printf("%f ", (num1 / num2));
+    printf("%f divided by %f is:\n", num1,num2); // Put this in a nice readable form.
+    printf("%f ", (num1 / num2)); // Use the / operator to divide the two numbers. Print the result.
     
-    if (num1 > num2)
+    if (num1 > num2) // Is num1 is larger than num2?
     {
         printf("\nThis fraction is top-heavy!\n");
     }
-    else if (num2 > num1)
+    else if (num2 > num1)// Is num2 is larger than num1?
     {
         printf("\nThis fraction is bottom-heavy!\n");
     }
-    else
+    else // Must be that the numbers are equal.
     {
         printf("\nThis fraction is equal!\n");
     }
